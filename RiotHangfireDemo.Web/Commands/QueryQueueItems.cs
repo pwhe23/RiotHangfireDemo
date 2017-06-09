75b98e1b944ceba845c80b43d96e60798adda5ed
Paul Wheeler <paul@paulwheeler.com>
2017-06-09 14:24:06 -0400

    MASTER Paging added

--- RiotHangfireDemo.Web/Commands/QueryQueueItems.cs
+++ RiotHangfireDemo.Web/Commands/QueryQueueItems.cs
@@ -1,14 +1,15 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using MediatR;
 
 namespace RiotHangfireDemo
 {
-    public class QueryQueueItems : IRequest<List<QueueItem>>
+    public class QueryQueueItems : IRequest<PagedList<QueueItem>>, IPageable
     {
         public string Status { get; set; }
+        public int? PageNumber { get; set; }
+        public int? PageSize { get; set; }
 
-        internal sealed class Handler : IRequestHandler<QueryQueueItems, List<QueueItem>>
+        internal sealed class Handler : IRequestHandler<QueryQueueItems, PagedList<QueueItem>>
         {
             private readonly DemoDb _db;
 
@@ -17,7 +18,7 @@ namespace RiotHangfireDemo
                 _db = db;
             }
 
-            public List<QueueItem> Handle(QueryQueueItems cmd)
+            public PagedList<QueueItem> Handle(QueryQueueItems cmd)
             {
                 var queueItems = _db
                     .QueueItems
@@ -30,7 +31,8 @@ namespace RiotHangfireDemo
                 }
 
                 return queueItems
-                    .ToList();
+                    .OrderByDescending(x => x.Id)
+                    .ToPagedList(cmd);
             }
         };
     };
---
+++ RiotHangfireDemo.Web/Utility/PagedList.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RiotHangfireDemo
+{
+    public interface IPageable
+    {
+        int? PageNumber { get; set; }
+        int? PageSize { get; set; }
+    };
+
+    public class PagedList<T>
+    {
+        public PagedList(IEnumerable<T> items, int pageNumber, int totalItems)
+        {
+            Items = items ?? new T[0];
+            PageNumber = pageNumber;
+            TotalItems = totalItems;
+        }
+
+        public IEnumerable<T> Items { get; }
+        public int PageNumber { get; }
+        public int TotalItems { get; }
+    };
+
+    public static class PagedList
+    {
+        public static PagedList<T> ToPagedList<T>(this IOrderedQueryable<T> query, IPageable cmd)
+        {
+            if (!cmd.PageNumber.HasValue || cmd.PageNumber < 1)
+                cmd.PageNumber = 1;
+
+            if (!cmd.PageSize.HasValue)
+                cmd.PageSize = 10;
+
+            var recordsToSkip = cmd.PageNumber > 1 ? (cmd.PageNumber.Value - 1) * cmd.PageSize.Value : 0;
+
+            var result = query
+                .Skip(recordsToSkip)
+                .Take(cmd.PageSize.Value)
+                .GroupBy(x => new { Total = query.Count() })
+                .FirstOrDefault();
+
+            return new PagedList<T>(result, cmd.PageNumber.Value, result?.Key.Total ?? 0);
+        }
+    };
+}
